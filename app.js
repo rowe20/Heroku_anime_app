cdd17760cf243b1acdcd6779ee891fa2c2a13d68
rowe20 <69379504+rowe20@users.noreply.github.com>
2021-12-04 22:09:02 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 5000;
+const mongoose = require("mongoose");
+
+mongoose
+.connect(process.env.MONGOURL)
+.then( ()=> console.log("Mongo DB connected"));
+
+//routes
+const animeRoute = require("./routes/animeRoute");
+const episodeRoute = require("./routes/episodeRoute");
+
+
+app.get("/",(req,res)=> res.send("Welcome To Anime World"));
+app.use("/anime",animeRoute);
+app.use("/episode",episodeRoute);
+
+app.listen(port, () => console.log(`Example app listening on port 5000!`))
